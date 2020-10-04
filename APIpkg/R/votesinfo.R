45e22a78ffeee2dd4739372158b83e53f9ad55a9
KANGBO CHEN <oliver_chenkb@qq.com>
2020-10-04 13:46:20 +0200

    new version

--- APIpkg/R/votesinfo.R
+++ APIpkg/R/votesinfo.R
@@ -5,11 +5,9 @@
 #'
 #' @return vote information of certain member
 #'
-#'
-#' @import httr
-#' @import rjson
-#' @importFrom  httr content GET
-#' @importFrom  rjson fromJSON
+#' @import methods
+#' @importFrom  jsonlite fromJSON
+#' @importFrom  httr GET content
 #' @examples
 #' x <- vott()
 #' cal_info(x,A Eriksson, Erik  (C))
@@ -17,18 +15,13 @@
 #'
 
 #unfinished. need some input
-vott <- function(){
-
-  #load package httr and jsonlite
-  # library(httr)
-  # library(jsonlite)
+vott <- function(name1){
 
   #get the URL
   vot_url <- "http://data.riksdagen.se/voteringlista/?bet=&punkt=&valkrets=&rost=&iid=&sz=500&utformat=json&gruppering=namn"
-   vot_url2 <- "http://data.riksdagen.se/voteringlista/?bet=&punkt=&valkrets=&rost=&iid=&sz=1000&utformat=json&gruppering=namn"
+  # vot_url2 <- "http://data.riksdagen.se/voteringlista/?bet=&punkt=&valkrets=&rost=&iid=&sz=1000&utformat=json&gruppering=namn"
 
   vot <- GET(vot_url)
-
   #convert the format from json to R
   vot_data <- fromJSON(content(vot,"text"))
 
@@ -37,8 +30,8 @@ vott <- function(){
   nam <- vot_data$voteringlista$votering$namn
   approve <- as.numeric(vot_data$voteringlista$votering$Ja)
   nay <- as.numeric(vot_data$voteringlista$votering$Nej)
-  absent <- as.numeric(vot_data$voteringlista$votering[["Frånvarande"]])
-  abstain <- as.numeric(vot_data$voteringlista$votering[["Avstår"]])
+  absent <- as.numeric(vot_data$voteringlista$votering[,4])
+  abstain <- as.numeric(vot_data$voteringlista$votering[,5])
 
   #calculate the attendance rate, attitude and participation
   pon = pap = attend <- NULL
@@ -50,35 +43,47 @@ vott <- function(){
       pap[i] <- abstain[i]/(approve[i]+nay[i])
 
       # attendence rate of the member
-      attend[i] <- 1-absent[i]/(approve[i]+nay[i]+abstain[i])
+      attend[i] <- 1-absent[i]/(approve[i]+nay[i]+abstain[i]+absent[i])
     }
 
-  # make it into a dataframe
-  df <- data.frame(cbind(nam,approve,nay,absent,abstain,pon,pap,attend))
-  attr(df,"class") <- "vott"
-  return(df)
-}
-#' @rdname cal_info
-#' @method cal_info vott
-#' @export
-
-#define S3method
-cal_info <- function(x){
-  UseMethod('cal_info')
-}
-
-#call the information of certain members (unfinished)
-cal_info.vott <- function(x,name1){
-  result <- NULL
   for(i in 1:length(vot_data$voteringlista$votering$namn)){
-    if(nam[i] == name1){
+    if(nam[i]==name1){
       name_mem <- nam[i]
-      result <- as.vector(x[i,])
-      cat(result)
+      approve_mem <- approve[i]
+      nay_mem <- nay[i]
+      absent_mem <- absent[i]
+      abstain_mem <- abstain[i]
+      pon_mem <- pon[i]
+      pap_mem <- pap[i]
+      attend_mem <- attend[i]
       break
     }
   }
+
+  # make it into a dataframe
+  l <- list(name=name_mem,approve=approve_mem,nay=nay_mem,absent=absent_mem,abstain=abstain_mem,pon=pon_mem,pap=pap_mem,attend=attend_mem)
+  attr(l,"class") <- "vott"
+  return(l)
 }
+
+#' @rdname calinfo
+#' @method calinfo vott
+#' @export
+#'
+#define S3method
+calinfo <- function(x){
+  UseMethod('calinfo')
+}
+
+#call the information of certain members (unfinished)
+calinfo.vott <- function(x){
+  # result <- NULL
+  # name_tem <- x$name
+  # app_tem <- x$approve
+  # result <- as.data.frame(cbind(name_tem,app_tem))
+   return(x$name,x$approve)
+}
+
 
 #cannot get the package function??
 
